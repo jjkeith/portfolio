9b873721cf366de1e5ad7b7d5543909bb6ab9c73
JJ <jj@jjkeith.net>
2016-07-29 12:40:49 -0700

    Added images for portfolio

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-(function($) {
-		$(function() {
-
-$('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    }
-  );
-
-		}); // End Document Ready
-})(jQuery); // End of jQuery name space
-
-// $(document).ready(function(){
-//   $(".dropdown-button").dropdown();
-//   $('.parallax').parallax();
-// });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
