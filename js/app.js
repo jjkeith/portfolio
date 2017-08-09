3df43b9689b9defd22ede629459c97232f0edae8
JJ <jj@jjkeith.net>
2017-08-08 19:25:58 -0700

    Revamped site

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-$(document).ready(function(){
-  $( '.button-collapse' ).sideNav({
-      edge: 'left',
-      closeOnClick: true
-    }
-  );
-  $( '.button-collapse' ).sideNav( 'hide' );
-});
-
-
 // Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
